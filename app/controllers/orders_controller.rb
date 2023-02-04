d97e2b29c7ba30f16fc97260dbf99e734689b416
tkhstty <tkhstty@gmail.com>
2023-02-04 11:55:03 +0900

    first commit

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,31 @@
+class OrdersController < ApplicationController
+  def index
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      pay_item
+      @order.save
+      redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+
+  private
+  def order_params
+    params.require(:order).permit(:price).merge(token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amoumt: order_params[:price],
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
+end
